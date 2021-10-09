5e0e90d0b51101c5973a6397b6099e3849453ce6
nanak <nanaksethh@gmail.com>
2021-10-10 04:20:18 +0530

    devserver changes

--- config/policies/givingaccess.js
+++ config/policies/givingaccess.js
@@ -1,15 +1,15 @@
 module.exports =async (ctx,next)=>{
     if(ctx.state.user){
       if(!ctx.params.id){
-          return ctx.unauthorized("You cant access it ")
-  
+          return ctx.unauthorized("You cant access it please pass id to continue ")
+
       }
       const targetUser=ctx.params.id.toString()
       const logedinuser=ctx.state.user.id.toString()
       if(targetUser===logedinuser){
           return await next()
       }
-       else return ctx.unauthorized('Your cant access this data')
+       else return ctx.unauthorized('Your cant access this data you are not the owner of this doc')
     }
   return ctx.unauthorized('You are not loged in')
       }
